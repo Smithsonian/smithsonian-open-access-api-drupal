34d28ad85410aa441c3118e781952bbd19abe36b
Edward Monk <edwardmonk@users.noreply.github.com>
2023-08-17 08:52:26 -0400

    enhance admin screen with more endpoint checks

--- js/form_behaviors.js
+++ js/form_behaviors.js
@@ -1,25 +1,30 @@
 (function ($, Drupal) {
-  Drupal.behaviors.clearSearchResults = {
-    attach: function (context, settings) {
-      // Disable the submit button initially
-      $('input[name="op"]').prop('disabled', true);
+    Drupal.behaviors.clearSearchResults = {
+        attach: function (context, settings) {
+            // Disable the submit button initially.
+            $('input[name="op"]').prop('disabled', true);
 
-      // Clear search results when user input values change.
-      $('select[name="endpoint"], select[name="category"], input[name="term"], input[name="query"]', context).once('clearSearchResults').on('input', function () {
-        var submitButton = $('input[name="op"]');
-        var searchResults = $('#search-results');
+            // Clear search results when user input values change.
+            $('select[name="endpoint"], select[name="category"], input[name="term"], input[name="query"]', context).once('clearSearchResults').on(
+                'input',
+                function () {
+                    let submitButton  = $('input[name="op"]');
+                    let searchResults = $('#search-results');
 
-        // Disable the submit button
-        submitButton.prop('disabled', true);
+                    // Disable the submit button.
+                    submitButton.prop('disabled', true);
 
-        // Clear the search results
-        searchResults.empty();
+                    // Clear the search results.
+                    searchResults.empty();
 
-        // Enable the submit button once clearing is complete
-        searchResults.promise().done(function () {
-          submitButton.prop('disabled', false);
-        });
-      });
-    }
-  };
+                    // Enable the submit button once clearing is complete.
+                    searchResults.promise().done(
+                        function () {
+                            submitButton.prop('disabled', false);
+                        }
+                    );
+                }
+            );
+        }
+    };
 })(jQuery, Drupal);
